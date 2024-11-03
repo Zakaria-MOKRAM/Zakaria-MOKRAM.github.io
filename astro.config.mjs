1954a8d45c37b094d8925667fb7355fd4896c452
Zakaria-MOKRAM <zakaria.mokramhtml@gmail.com>
2024-11-03 20:54:30 +0100

    final commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/Zakaria-MOKRAM/Zakaria-MOKRAM.github.io.git'
+
 });
